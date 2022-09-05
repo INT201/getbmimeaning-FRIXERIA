c1069cf72866b210a321358b8831832bfb6649fc
FRIXERIA <pattharaphon.saef@kmutt.ac.th>
2022-09-05 23:02:10 +0700

    update#3

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,30 +1,15 @@
-//const { template } = require('@babel/core')
+const { template } = require('@babel/core')
+let bmi
+if (bmi<18.5) {
+  return ("Underweight")
+} else if (bmi<25) {
+    return ("Normal Weight")
+} else (bmi>25) 
+  return ("Overweight") 
 function calculateBMI(weight, height) {
   return weight/((height*height))
 }
-
 function getBMIMeaning(weight, height) {
-let BMI=calculateBMI()
-  switch (BMI) {
-    case BMI<18.5:
-      BMI ="Underweight";
-      break;
-    case BMI<25:
-      BMI = "Normal weight";
-      break;
-    case BMI>25:
-      BMI = "Overweight";
-      break;
-  }
-  // if (BMI<18.5) {
-  //   return ("Underweight")
-  // } else if (BMI<25) {
-  //     return ("Normal Weight")
-  // } else (BMI>25) 
-  //   return ("Overweight")   
-  }
-       
-  
-console.log(getBMIMeaning(65, 1.8))
-console.log(calculateBMI(65, 1.8))
-//module.exports = getBMIMeaning
+  return calculateBMI
+}
+module.exports = getBMIMeaning
